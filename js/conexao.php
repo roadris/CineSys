bea56cff0b5f6205322b7770f7f980ebea96de44
Jeferson <jefersonebs@gmail.com>
2014-05-05 16:06:27 -0300

    Create conexao.php

---
+++ js/conexao.php
@@ -0,0 +1,30 @@
+<?php
+
+	$host = "localhost";
+	$user = "postgres";
+	$pswd = "J96914333";
+	$dbname = "TestAjax";
+	$con = null;
+
+	function abrirConexao($con, $host, $user, $pswd, $dbname)
+	{
+		$con = pg_connect("host='$host' dbname='$dbname' user='$user' password='$pswd'");
+		return $con;
+	}
+
+	function fecharConexao($con)
+	{
+		pg_close($con);
+	}
+
+	function testarConexao($con)
+	{
+		if(!$con)
+		{
+			echo "<h3>O sistema não está conectado à  [$dbname] em [$host].</h3>";
+			exit;
+		}else{
+			echo "<h3>O sistema está conectado à  [$dbname] em [$host].</h3>";
+		}
+	}
+?>
